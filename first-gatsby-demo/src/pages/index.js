c0f9f4e1fd41de52513dda9971cdd24b880aeb78
Monicacholico <monicachol@hotmail.com>
2020-02-27 22:59:23 -0600

    added page and style

--- first-gatsby-demo/src/pages/index.js
+++ first-gatsby-demo/src/pages/index.js
@@ -9,13 +9,14 @@ const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
     <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
+    <p>Welcome to my First Gatsby Demo site.</p>
     <p>Now go build something great.</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
-    <Link to="/page-2/">Go to page 2</Link>
+    <Link to="/about/">About</Link>
   </Layout>
+  
 )
 
 export default IndexPage
